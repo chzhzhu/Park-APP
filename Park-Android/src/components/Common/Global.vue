a9471edd7be6f79d86e0e61c6faffc42c377b980
MaMaoting <mmtdiana@outlook.com git config --global user.name mmt>
2019-04-17 17:32:24 +0800

    04.17 5.32

---
+++ Park-Android/src/components/Common/Global.vue
@@ -0,0 +1,10 @@
+<script>
+  const USERNAME = '';
+  const PASSWORD = '';
+  const DOMAIN = 'http://localhost:8083/parkingApp/rest/';
+  export default{
+      USERNAME,
+      PASSWORD,
+      DOMAIN
+    }
+</script>
